af64f9269ba30af698648b4158826b3a7691d64d
ijun17 <54887575+ijun17@users.noreply.github.com>
2020-07-23 23:20:37 +0900

    Update output.js

--- src/js/output.js
+++ src/js/output.js
@@ -2,7 +2,7 @@ const input2 = document.querySelector(".input"),
     outout = document.querySelector(".output"),
     outputBtn = document.querySelector(".js-outputBtn");
 
-function replaceFormula(formula2){
+function replaceFormula(formula2){ //곱하기와 나누기 연산자를 교체
     let formula=formula2;
     while(formula.indexOf("×")>0)formula = formula.replace("×","*");
     while(formula.indexOf("÷")>0)formula = formula.replace("÷","/");
@@ -10,12 +10,12 @@ function replaceFormula(formula2){
 }
 
 
-function outputFormula(event){
+function outputFormula(event){ //입력 수식의 결과값을 출력
     let formula = input2.innerText;
     formula = replaceFormula(formula);
     const err = new Error('invalid formula');
     try{
-        output.innerText = eval(formula);
+        output.innerText = eval(formula);//스트링을 코드로
     }catch(err){
         output.innerText="E";
         console.log(formula);
