ccd6b8a2901f0b46178b00f77e50dc02dd75851b
ijun17 <54887575+ijun17@users.noreply.github.com>
2020-07-23 23:22:45 +0900

    Update input.js

--- src/js/input.js
+++ src/js/input.js
@@ -2,7 +2,7 @@ const btn = document.querySelector(".btn"),
     input = document.querySelector(".input"),
     output = document.querySelector(".output");
 
-function deleteFormula(isC){
+function deleteFormula(isC){ //C와 CE 수행
     output.innerText="";
     if(isC){
         input.innerText="";
@@ -13,16 +13,16 @@ function deleteFormula(isC){
     }
 }
 
-function inputFormula(value){
+function inputFormula(value){ //숫자 또는 연산자 입력(등호 제외)
     output.innerText="";
     if(value!==undefined)
         input.innerText = input.innerText+`${value}`;
 }
 
-function clickBtn(event){
+function clickBtn(event){ //테이블을 클릭했을 때
     const et = (String)(event.target);
     const value = event.target.innerText;
-    if(et.indexOf("Button")==-1){
+    if(et.indexOf("Button")==-1){ //클릭한게 버튼일 때
         console.log(et);
     }else if(value=="C"){
         deleteFormula(true);
