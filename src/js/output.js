5d20d83616bf3c9a1e800dc7282050792f5f9f8f
ijun17 <ijun17@naver.com>
2020-07-27 12:23:45 +0900

    연속된 나누기 기호에 따른 오류 수정

--- src/js/output.js
+++ src/js/output.js
@@ -2,8 +2,22 @@ const input2 = document.querySelector(".input"),
     outout = document.querySelector(".output"),
     outputBtn = document.querySelector(".js-outputBtn");
 
-function replaceFormula(formula2){ //곱하기와 나누기 연산자를 교체
+function replaceFormula(formula2){ //×와 ÷를 코드상에서 수행되는 *와/로 교체
     let formula=formula2;
+    //나누기 연산자가 연속되어 주석처리가 되는걸 방지
+    let isSeqOpr=false;
+    for(let i=0; i<formula.length; i++){
+        if(formula[i]=="÷"){
+            if(isSeqOpr==true){
+                formula="E";
+                break;
+            }
+            isSeqOpr=true;
+        }else{
+            isSeqOpr=false;
+        }
+    }
+    //나머지 기능 수행
     while(formula.indexOf("×")>0)formula = formula.replace("×","*");
     while(formula.indexOf("÷")>0)formula = formula.replace("÷","/");
     return formula;
